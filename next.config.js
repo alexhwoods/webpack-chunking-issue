00ee58d8e76d5cb6d64a9c61a5363e0d44a8d39b
Alex Woods <alexhwoods@gmail.com>
2021-10-12 16:12:24 -0400

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  optimizeFonts: false,
+  webpack(config, options) {
+    if (options.dev || options.isServer) return config;
+
+    /* mess around here */
+
+    return config;
+  },
+});
