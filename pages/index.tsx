d41266c8b0d97c9cfa7127ef63f6dda9ea4f2e1d
Alex Woods <alexhwoods@gmail.com>
2021-10-13 07:28:39 -0400

    Remove state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import type { NextPage } from "next";
 
 import { getComponent } from "../components/getComponent";
 const Component = getComponent("Foo");
 
 const Home: NextPage = () => {
-  const [viewShip, setViewShip] = useState(false);
   return (
     <div>
       <Component />
